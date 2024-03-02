8dfcb51198f61c594c509deee6530288fa83d8c6
Pekar7 <k1901321@gmail.com>
2024-03-03 02:56:58 +0300

    add active profile conditional

---
+++ src/main/java/ru/edi/edi_integration/cfg/DevProfileCondition.java
@@ -0,0 +1,27 @@
+package ru.edi.edi_integration.cfg;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * Добавляем DevProfileCondition чтобы отключить KafkaListener
+ * Для local - отключить, для dev - включить
+ */
+public class DevProfileCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        Environment environment = context.getEnvironment();
+        String conditionalContext = environment.getProperty("project.profile.conditional");
+        String[] activeProfiles = environment.getActiveProfiles();
+        for (String profile : activeProfiles) {
+            if (conditionalContext.equalsIgnoreCase(profile)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/ru/edi/edi_integration/service/kafka/KafkaListenerConditional.java
+++ src/main/java/ru/edi/edi_integration/service/kafka/KafkaListenerConditional.java
@@ -1,36 +1,37 @@
 package ru.edi.edi_integration.service.kafka;
 
-
-import org.springframework.context.annotation.Conditional;
-import ru.edi.edi_integration.cfg.DevProfileCondition;
-import ru.edi.edi_integration.enums.SenderCode;
-import ru.edi.edi_integration.service.EdifactService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import ru.edi.edi_integration.cfg.DevProfileCondition;
+import ru.edi.edi_integration.enums.SenderCode;
+import ru.edi.edi_integration.service.EdifactService;
 
 import java.util.List;
 
 @Service
 @Slf4j
-public class KafkaListenerService implements ConsumerSeekAware {
+@Conditional(DevProfileCondition.class)
+public class KafkaListenerConditional {
 
     private final EdifactService edifactService;
 
-    public KafkaListenerService(EdifactService edifactService) {
+    @Autowired
+    public KafkaListenerConditional(EdifactService edifactService) {
         this.edifactService = edifactService;
     }
 
     @Transactional(transactionManager = "tmKafkaMessage", rollbackFor = RuntimeException.class, propagation = Propagation.REQUIRES_NEW)
-    @KafkaListener(groupId = "${project.kafka.consumer.group.id}", topics = {"${project.kafka.module.topic.edifact.adapted.encrypt}"}, containerFactory = "batchFactoryInt")
-    public void edifactEncryptedInternalListener(List<ConsumerRecord<String, String>> messages) {
+    @KafkaListener(groupId = "${project.kafka.consumer.group.id}", topics = {"${project.kafka.module.topic.edifact.adapted.crypt}"}, containerFactory = "batchFactoryInt")
+    public void xmlInternalListener(List<ConsumerRecord<String, String>> messages) {
         try {
             for (ConsumerRecord<String, String> message : messages) {
-                log.info("encrypt");
+                log.info("crypt");
                 processEdifactListener(message, false);
             }
         } catch (Exception e) {
