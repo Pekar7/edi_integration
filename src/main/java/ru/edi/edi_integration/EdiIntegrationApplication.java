f1880e0267d402bf23c5a575f5c4c552d5504962
Петросян Карен Эдуардович <petrosyan_ke@nlmk.com>
2025-04-13 23:35:13 +0300

    Продвинутое проксирование

--- src/main/java/ru/edi/edi_integration/EdiIntegrationApplication.java
+++ src/main/java/ru/edi/edi_integration/EdiIntegrationApplication.java
@@ -2,12 +2,12 @@ package ru.edi.edi_integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.javavit.proxy", "ru.edi.edi_integration"})
 @SpringBootApplication
 public class EdiIntegrationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EdiIntegrationApplication.class, args);
     }
-
 }
--- src/main/java/ru/edi/edi_integration/controllers/HelloController.java
+++ src/main/java/ru/edi/edi_integration/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package ru.edi.edi_integration.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 import ru.edi.edi_integration.service.HelloServiceHandler;
 
@@ -13,7 +14,8 @@ public class HelloController {
         this.helloServiceHandler = helloServiceHandler;
     }
 
-    @GetMapping("/proxy/karolina/{karolina}")
+    @Operation(summary = "Send hello to someone", operationId = "handleHello")
+    @PostMapping("/proxy/karolina/{karolina}")
     public String callKarolina(@PathVariable String karolina) {
         return helloServiceHandler.handleHello(karolina);
     }
