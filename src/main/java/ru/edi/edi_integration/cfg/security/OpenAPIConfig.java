2b2a3ad313f7fbf812e2e30fc80f152481ca7e04
Петросян Карен Эдуардович <petrosyan_ke@nlmk.com>
2025-04-20 23:48:35 +0300

    Модификация keycloak

--- src/main/java/ru/edi/edi_integration/cfg/security/OpenAPIConfig.java
+++ src/main/java/ru/edi/edi_integration/cfg/security/OpenAPIConfig.java
@@ -3,11 +3,18 @@ package ru.edi.edi_integration.cfg.security;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
+
+    @Value("${spring.doc.swagger-ui.oauth2.authorization-url}")
+    private String authorizationUrl;
+
+    @Value("${spring.doc.swagger-ui.oauth2.token-url}")
+    private String tokenUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -17,8 +24,8 @@ public class OpenAPIConfig {
                                 .type(SecurityScheme.Type.OAUTH2)
                                 .flows(new OAuthFlows()
                                         .authorizationCode(new OAuthFlow()
-                                                .authorizationUrl("http://localhost:8080/realms/edi/protocol/openid-connect/auth")
-                                                .tokenUrl("http://localhost:8080/realms/edi/protocol/openid-connect/token")
+                                                .authorizationUrl(authorizationUrl)
+                                                .tokenUrl(tokenUrl)
                                                 .scopes(new Scopes().addString("openid", "OpenID scope"))
                                         )
                                 )
